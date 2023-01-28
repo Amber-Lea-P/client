4ef4cc9b66cfb708a39fa1172e852a5c4320e1c6
Amber-Lea-P <amber.pontinen@gmail.com>
2023-01-28 09:40:03 +0200

    updated file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import  Header from "./components/core/Header";
-import Landing from "./components/core/Landing";
-import Footer from "./components/core/Footer";
+
 
 
 function App() {
   return (
     <div>
-    <Header></Header>
-    <Landing></Landing>
-    <Footer></Footer>
+    
     </div>
   );
 }
