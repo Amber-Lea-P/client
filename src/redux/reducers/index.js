e164c8281a98799e5b6bfc7e94cc4480f6dece9f
Amber-Lea-P <amber.pontinen@gmail.com>
2023-02-12 12:31:08 +0200

    index.js rootReducer defining

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,9 @@
-//rootreducer
+//rootreducer : its going to hold all reducer details at one place
+import { combineReducers } from 'redux';
+
+const rootReducers = combineReducers({
+  example: () => [],
+});
+
+export default rootReducers;
+
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,15 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducers from "../reducers";
 
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+const store = createStore(rootReducers,
+    initialState, 
+    composeWithDevTools(applyMiddleware(...middleware))
+    );
 
 //Three requirements:
 // root reducer : index.js from reducer folder will hold details about your all reducers
