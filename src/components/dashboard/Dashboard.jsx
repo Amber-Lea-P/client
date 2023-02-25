3e8ff67a80e6542b998532e7099834e47f6a7546
Amber-Lea-P <amber.pontinen@gmail.com>
2023-02-25 13:27:50 +0200

    dashboard and createprofile jsx.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,22 +1,57 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import { connect } from 'react-redux'
-import DashboardAction from './DashboardAction'
+import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import DashboardAction from "./DashboardAction";
+import { getCurrentProfile } from "../../redux/actions/profileAction";
+import { Link } from "react-router-dom";
+export const Dashboard = ({
+  getCurrentProfile,
+  profileReducer: { profile },
+  auth: { user },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
 
-export const Dashboard = (props) => {
+  const successPart = (
+    <>
+      {" "}
+      <DashboardAction></DashboardAction>Exp Edu
+    </>
+  );
+  const failurePart = (
+    <>
+      {" "}
+      <p>You have not yet setup a profile, please add some info</p>
+      <Link to="/create-profile" className="btn btn-primary my-1">
+        Create Profile
+      </Link>
+    </>
+  );
   return (
-    <div>Welcome UserName
-    <DashboardAction></DashboardAction>
+    <div>
+      <section className="container">
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user" /> Welcome {user && user.name}
+        </p>
+        {profile !== null ? successPart : failurePart}
+      </section>
     </div>
-  )
-}
-
-Dashboard.propTypes = {
-  
+  );
 };
 
-const mapStateToProps = (state) => ({})
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profileReducer: PropTypes.object.isRequired,
+};
 
-const mapDispatchToProps = {}
+const mapStateToProps = (state) => ({
+  auth: state.authReducer,
+  profileReducer: state.profileReducer,
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
+const mapDispatchToProps = { getCurrentProfile };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
